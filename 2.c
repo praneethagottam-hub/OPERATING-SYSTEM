56b13fabb43c557b9fe8ed735bdc5af2549f6f4e
praneethagottam-hub <praneethagottam@gmail.com>
2026-02-11 09:43:45 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+ int i, j, n;
+ printf("Enter the number of rows: ");
+ scanf("%d", &n);
+ for(i=0;i<n;i++) {
+  for(j=0;j<=i;j++) {
+   printf(" %d ", j+1);
+  }
+  printf("\n");
+ }
+ return 0;
+}
